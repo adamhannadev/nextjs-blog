ddfdbc94afce3344702fa0108eef20112a0eefaa
adamhannadev <46306810+adamhannadev@users.noreply.github.com>
2023-01-30 14:46:49 +0000

    Add logo and personalize title globals

--- components/Header.js
+++ components/Header.js
@@ -1,9 +1,16 @@
 import Link from 'next/link';
+import Image from 'next/image';
+import logoPic from '../adam.jpeg';
 
 export default function Header({ name }) {
   return (
     <header className="pt-20 pb-12">
-      <div className="w-12 h-12 rounded-full block mx-auto mb-4 bg-gradient-conic from-gradient-3 to-gradient-4" />
+      <Image 
+        src={logoPic}
+        width={200}
+        height={200}
+        class="rounded-full"
+      />
       <p className="text-2xl dark:text-white text-center">
         <Link href="/">
           <a>{name}</a>
--- utils/global-data.js
+++ utils/global-data.js
@@ -1,10 +1,10 @@
 export const getGlobalData = () => {
   const name = process.env.BLOG_NAME
     ? decodeURI(process.env.BLOG_NAME)
-    : 'Jay Doe';
+    : 'Adam Hanna';
   const blogTitle = process.env.BLOG_TITLE
     ? decodeURI(process.env.BLOG_TITLE)
-    : 'Next.js Blog Theme';
+    : 'Adam Hanna\'s Adventures in Programming';
   const footerText = process.env.BLOG_FOOTER_TEXT
     ? decodeURI(process.env.BLOG_FOOTER_TEXT)
     : 'All rights reserved.';
